bb282356fec2a09424e55e910748b521a088220a
Hack Reactor Student <student@hackreactor.com>
2018-11-28 16:32:54 -0800

    Passing all actions tests

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,22 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
   //TODO:  Write an asynchronous action to handle a video search!
+  let options = {
+    key: YOUTUBE_API_KEY,
+    query: q,
+    max: 5
+  }
+
+  let videos = searchYouTube(options, (data) => {
+    return data;
+  });
+
+  return (dispatch) => {
+    dispatch(changeVideoList(videos))
+  };
 };
 
 export default handleVideoSearch;
+
+
